6de0149c37fe4f4198e14d2154e1b961678f0c27
Murat Öztürk <40055791+muratoeztuerk@users.noreply.github.com>
2018-10-20 19:30:02 +0200

    Update FibonacciSequence.java

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,33 +1,33 @@
-/*
-
-In mathematics, the Fibonacci numbers are the numbers in the following 
-integer sequence, called the Fibonacci sequence, and characterized by the fact 
-that every number after the first two is the sum of the two preceding ones:
-1 , 1 , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55 , 89 , 144.... [WIKIPEDIA]
-
- */
-package fibonaccisequence;
-
-/**
- *
+/** 
+ * 
+ * In mathematics, the Fibonacci numbers are the numbers in the following 
+ * integer sequence, called the Fibonacci sequence, and characterized by the fact 
+ * that every number after the first two is the sum of the two preceding ones:
+ * 1 , 1 , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55 , 89 , 144.... [WIKIPEDIA]
+ * 
  * @author Murat Öztürk
+ * 
+ * 
  */
+
+
 public class FibonacciSequence {
 
-    public static void FibonacciNumbers(int first, int second, int last) {
+	public static void main(String[] args) {
 
-        second = first + second;
+		for (int i = 0; i < 13; i++) {
+			System.out.println(FibonacciNumbers(i));
+		}
 
-        if (second <= last) {
-            System.out.println(second);
-            FibonacciNumbers(second, first, last);
-        }
-    }
+	}
 
-    public static void main(String[] args) {
+	static int FibonacciNumbers(int n) {
 
-        FibonacciNumbers(0, 1, 144);
+		if (n <= 1)
+			return n;
+		else
+			return FibonacciNumbers(n - 1) + FibonacciNumbers(n - 2); // F(n) = F(n-1) + F(n-2)
 
-    }
+	}
 
 }
